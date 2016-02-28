b9d33c6462579d22f59af90c04cac3d17f03a281
Ryan Gerstenkorn <ryan_gerstenkorn@fastmail.fm>
2016-02-28 07:02:47 -0600

    Create FTB server

--- cookbooks/my_minecraft_server/recipes/default.rb
+++ cookbooks/my_minecraft_server/recipes/default.rb
@@ -12,9 +12,8 @@
 # actual username
 # node.override['minecraft']['ops'] = %w(<your mojang account>)
 
-node.override['sds-dns-caching']['dnsmasq']['servers'] = %w(8.8.8.8 8.8.4.4)
 
-node.override['minecraft']['install_type'] = 'bukkit'
+node.override['sds-dns-caching']['dnsmasq']['servers'] = %w(8.8.8.8 8.8.4.4)
 
 include_recipe 'base-server::apt'
 include_recipe 'base-server::users' if node['admin']
@@ -22,7 +21,11 @@ include_recipe 'base-server::dns-caching'
 include_recipe 'base-server::misc'
 
 
-node.override['java']['install_flavor'] = 'openjdk'
+## Uncomment the following line to create a bukkit, spigot or ftb server
+# node.override['minecraft']['install_type'] = 'bukkit'
+# node.override['minecraft']['install_type'] = 'spigot'
+node.override['minecraft']['install_type'] = 'ftb'
 
+# The type of minecraft server can be set in the default attributes file of this cookbook
 include_recipe 'minecraft::default'
 
